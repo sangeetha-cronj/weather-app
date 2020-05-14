fcdd2f577ee9479788bfb5d215f98f2866f8e5a4
sangeetha-cronj <sangeetha@cronj.com>
2020-05-14 19:31:03 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=3f46db4f8714f6ebc4f6c98533d453e7&query=' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.cloudcover + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
